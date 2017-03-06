fe4e2774296534ee61c7bab30615fde2c1b7306d
edess <elderakpa@edesss-macbook-pro.local>
2017-03-06 22:41:30 +0900

    Initial Commit

---
+++ LetDoIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LetDoIt
+//
+//  Created by Edess Akpa on 3/6/17.
+//  Copyright © 2017 The Elder Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
